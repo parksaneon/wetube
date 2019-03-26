0a6164b3c64e2fa8236ea02b6e2823bf779959ec
parksaneon <47975991+parksaneon@users.noreply.github.com>
2019-03-26 23:39:42 +0900

    03.26

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,14 +1,22 @@
 import express from "express";
 import routes from "../routes";
-import { editVideo, deleteVideo, videoDetail, getUpload, postUpload } from "../controllers/videoController";
+import { getEditVideo, deleteVideo, videoDetail, getUpload, postUpload, postEditVideo } from "../controllers/videoController";
 import { uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
 
+// Upload
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
+
+// Video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.eidtVideo, editVideo);
+
+// Edit Video
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+// Delete Video
 videoRouter.get(routes.deleteVideo, deleteVideo);
 
 
